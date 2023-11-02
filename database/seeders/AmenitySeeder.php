bc84eacd58ef18635c03d341858ec062454349ec
MicheleGresta <malachia_1997@hotmail.it>
2023-11-02 12:56:53 +0100

    testing seeder amenities

--- database/seeders/AmenitySeeder.php
+++ database/seeders/AmenitySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Amenity;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,130 @@ class AmenitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $amenities = [ 
+            $bathroom = [
+                [
+                    "name" => "Bath tub",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Hair dryer",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Cleaning products",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Hot water",
+                    "icon" => "x"
+                ]
+                ],
+            
+            $bedroom = [
+                [
+                    "name" => "Free washer – In unit",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Hangers",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Clothes drying rack",
+                    "icon" => "x"
+                ]
+                ],
+            
+            $entertainment = [
+                [
+                    "name" => "Ethernet connection",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "TV",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Sound system with Bluetooth and aux",
+                    "icon" => "x"
+                ]
+                ],
+            
+            $family = [
+                [
+                    "name" => "High chair",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Children’s tableware",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Board games",
+                    "icon" => "x"
+                ]
+                ],
+            
+            $air_system = [
+                [
+                    "name" => "Indoor fireplace",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Central heating",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Radiant heating",
+                    "icon" => "x"
+                ]
+                ],
+            
+            $safety = [
+                [
+                    "name" => "Smoke alarm",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Children’s tableware",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Board games",
+                    "icon" => "x"
+                ]
+                ],
+            
+            $family = [
+                [
+                    "name" => "High chair",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Children’s tableware",
+                    "icon" => "x"
+                ],
+                [
+                    "name" => "Board games",
+                    "icon" => "x"
+                ]
+                ]
+            
+            
+            ];
+
+
+
+            foreach ($amenities as $category => $amenity){
+                foreach ($amenity as $service) {
+                    Amenity::create([
+                        "name" => $service["name"],
+                        "icon" => $service["icon"],
+                        "category" => key($amenity)
+                    ]);
+                }
+                
+            }
     }
 }
