55da33ca2969a29ff7329401b0d1ae40bb5d1123
Islam.Benguerba <islam.benguerba@gmail.com>
2023-11-10 17:02:24 +0100

    MostFilter in ApartmentController

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Database\Eloquent\Builder;
 use App\Http\Controllers\Controller;
+use App\Models\Amenity;
 use App\Models\Apartment;
+use App\Models\User;
+use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,7 +16,7 @@ class ApartmentController extends Controller
     //
     static $latitude = '';
     static $longitude = '';
-//funzione aritmetica, molto interessante ma non funzionante
+    //funzione aritmetica, molto interessante ma non funzionante
     public static function pointsWithinRadius($latitude, $longitude, $radius)
     {
         $presetRadius = 20;
@@ -50,6 +53,35 @@ class ApartmentController extends Controller
         return $data;
     }
 
+    //con esso in teoria dovrebbe filtrare per raggio e anche per sponsorizzazione!!
+    public static function megaFilter($latitude, $longitude, $radius){
+
+        $radiusMeters = $radius * 1000;
+        $query =" SELECT *
+        FROM `sponsorship_apartment`
+        JOIN `sponsorships` ON `sponsorship_apartment`.`sponsorship_id` = `sponsorships`.`id`
+        RIGHT OUTER JOIN `apartments` ON `sponsorship_apartment`.`apartment_id` = `apartments`.`id`
+        WHERE ST_Distance_Sphere(point(latitude, longitude), point(?, ?)) <= ?
+        ORDER BY `sponsorships`.`name` DESC";
+
+        $data =DB::select($query,[$latitude, $longitude, $radiusMeters]);
+        return $data;
+
+
+    }
+    public static function orderbyPayment()
+    {
+        $sponsorship_apartments = DB::table('sponsorship_apartment')
+            ->join('sponsorships', 'sponsorship_apartment.sponsorship_id', '=', 'sponsorships.id')
+            ->rightJoin('apartments', 'sponsorship_apartment.apartment_id', '=', 'apartments.id')
+            ->orderByDesc('sponsorships.name')
+            ->whereNull('deleted_at')
+            ->get();
+
+            return $sponsorship_apartments;
+    }
+
+
 
     public function index(Request $request)
     {
@@ -83,15 +115,21 @@ class ApartmentController extends Controller
             // $raggio = 80;
             //$risultati = ApartmentController::pointsWithinRadius($latitude, $longitude, $raggio);
             $risultati = ApartmentController::getDataWithinRadius($latitude, $longitude, $raggio);
+            //prova Megafilter
+            $appartamentiFiltrati = ApartmentController::megaFilter($latitude,$longitude,$raggio);
         }
+
         // Additional filter based on municipality
         // if (!empty($freeformAddress)) {
         //     $apartmentsQuery->where('address', 'LIKE', '%' . $freeformAddress . '%');
         // }
 
         $filteredApartments = $apartmentsQuery->get();
-            //solo per fini di Dev restituisco gli appartamenti totali e anche quelli filtrati con anche il raggio schelto.
-        return response()->json(['apartments' => $filteredApartments, 'dati' => $latitude, 'funzione' => $risultati,'raggio'=>$raggio]);
+        
+        //$appartamentiPaganti = ApartmentController::orderbyPayment();
+
+        //solo per fini di Dev restituisco gli appartamenti totali e anche quelli filtrati con anche il raggio scelto.
+        return response()->json(['apartments' => $filteredApartments,'funzione' => $risultati, 'raggio' => $raggio, 'consigliati' => $appartamentiFiltrati ]);
     }
 
     public function show($slug)
@@ -99,12 +137,27 @@ class ApartmentController extends Controller
         $showedApartmentQuery = Apartment::query();
 
         /* $selectedApartmentSlug = $request->input("selectedApartmentSlug"); */
-
+        /* selezioniamo un appartamento trammite Sulg mandato trammite chiamata axios*/
         $showedApartmentQuery->where("slug", $slug);
+        //recuperiamo anche le sue relazioni con la tabella amenities
+        $showedApartmentQuery->with(['amenities']);
+        /*ora con lo slug sempre usato per filtrare l'appartamento faciamo una query
+        per poter ricavare l'user_id di chi ha registrato l'appartamento  */
+        $utenti = DB::table('apartments')->where('slug', $slug)->select('user_id')->get();
+        // $ utenti sarà un array contenente un solo elemento poichè c'è un solo proprietario
+        // per recupeerarlo non possiamo fare $utenti[0]['user_id'] perche è un oggetto
+        $numero = $utenti[0]->user_id;
+        //$numero è un numero di riferenza del utente 
+        $utente = DB::table('users')->find($numero);
+        //cancello la passowrd per non restituirla in front-office
+        unset($utente->password);
+        //$Amenity =$showedApartmentQuery::with('amenities');
+
+
 
         $showedApartment = $showedApartmentQuery->get();
 
-        return response()->json(['singleApartment' => $showedApartment]);
+        return response()->json(['singleApartment' => $showedApartment, 'utente' => $utente]);
     }
 
     public function getPositions()
