de39f06ee043ca4dd59bf8af469364f26c57034d
acolombo09 <acolombo0911@gmail.com>
2023-11-13 15:16:11 +0100

    amenities-icons-added

--- database/migrations/2023_10_31_161234_create_amenities_table.php
+++ database/migrations/2023_10_31_161234_create_amenities_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string("name");
-            $table->string("icon");
+            $table->text("icon");
             $table->string("description");
             $table->string("category");
 
