25fbe8b978ba40fd1d3804c0ba2ff0ca2db08ddb
MicheleGresta <malachia_1997@hotmail.it>
2023-11-14 02:27:55 +0100

    finish

--- resources/views/admin/messages/message.blade.php
+++ resources/views/admin/messages/message.blade.php
@@ -2,17 +2,27 @@
 @section('content')
     <div class="container">
         <div class="row p-4 box-messages">
-            <div class="col">
+            <div class="col position-relative">
                 <h3 class="mb-4">Messages received</h3>
                 {{-- messages list --}}
-                @foreach ($messages->reverse() as $message)
-                <div class="box-message mb-1 p-3 ms-2 position-relative">
-                    <h4 class="py-1"><strong>From: </strong>{{ $message->name }}</h4>
-                    <p><strong>Content: </strong>{{ $message->message }}</p>
-                    <p class="box-time">{{$message->created_at}}</p>
-                </div>
-            @endforeach
+                <div class="box-list pt-4">
+                    @foreach ($messages->reverse() as $message)
+                        <div class="box-message px-3 ms-2">
+                            <div class="row">
+                                <div class="col-6 ps-4 d-flex flex-column justify-content-center">
+                                    <h4 class="py-1"><strong>From: </strong>{{ $message->name }}</h4>
+                                    <p><strong>Content: </strong>{{ $message->message }}</p>
+                                </div>
+                                <div class="col-6 d-flex align-items-center justify-content-end pe-5">
+                                    <p class="box-time m-0 p-0">{{ $message->created_at }}</p>
+                                </div>
+                            </div>
 
+
+                        </div>
+                        <hr class="mx-4">
+                    @endforeach
+                </div>
 
             </div>
 
@@ -20,14 +30,12 @@
     </div>
 
     <style lang="scss">
-        .box-message {
+        .box-time {}
+
+        .box-list {
             border: 2px solid rgb(14, 66, 179);
             border-radius: 1rem;
-            .box-time{
-                position: absolute;
-                right: .7rem;
-                bottom: -.5rem
-            }
         }
+
     </style>
 @endsection
